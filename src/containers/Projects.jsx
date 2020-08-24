371de7d97601536c59c54820550f70b7b97b642d
DianaMS <dmartinezs@uni.pe>
2020-08-24 02:02:00 -0500

    se agrega variable general como data

--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,27 +1,56 @@
 import React from 'react';
 import Item from '../components/Item';
-import pokemonGo from '../assets/static/diana.png';
 import '../assets/styles/components/Projects.scss';
+
+const giff = {
+  uno: 'src/assets/static/cardValidation.gif',
+  dos: 'src/assets/static/cipher.gif',
+  tres: 'src/assets/static/pokemonGo.gif',
+  cuatro: 'src/assets/static/mdLinks.gif',
+  cinco: 'src/assets/static/redSocial.gif',
+  seis: 'src/assets/static/burgerApi.gif',
+};
 
 const Projects = () => (
 
   <section className='container-projects'>
     <Item
-      img={pokemonGo}
+      img={giff.uno}
       title='POKEMON GO'
       description='Aqui va toda la descripción'
       buttonleft='README'
       buttonright='DESPLIEGUE'
     />
     <Item
-      img={pokemonGo}
+      img={giff.dos}
       title='POKEMON GO'
       description='Aqui va toda la descripción'
       buttonleft='README'
       buttonright='DESPLIEGUE'
     />
     <Item
-      img={pokemonGo}
+      img={giff.tres}
+      title='POKEMON GO'
+      description='Aqui va toda la descripción'
+      buttonleft='README'
+      buttonright='DESPLIEGUE'
+    />
+    <Item
+      img={giff.cuatro}
+      title='POKEMON GO'
+      description='Aqui va toda la descripción'
+      buttonleft='README'
+      buttonright='DESPLIEGUE'
+    />
+    <Item
+      img={giff.cinco}
+      title='POKEMON GO'
+      description='Aqui va toda la descripción'
+      buttonleft='README'
+      buttonright='DESPLIEGUE'
+    />
+    <Item
+      img={giff.seis}
       title='POKEMON GO'
       description='Aqui va toda la descripción'
       buttonleft='README'
