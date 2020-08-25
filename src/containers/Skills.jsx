ee47078d074b796c9a63b791925c69f0856f2230
DianaMS <dmartinezs@uni.pe>
2020-08-24 19:39:27 -0500

    síntesis de data con metodo .map()

--- src/containers/Skills.jsx
+++ src/containers/Skills.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import ItemSkills from '../components/ItemSkills';
+import data from '../data/data';
+import '../assets/styles/components/Skills.scss';
 
 const Skills = () => (
-  <p>Aquí va skills</p>
+  <section className='container-skills'>
+    <section className='title'>
+      <p className='title-skills'>COMPETENCIAS</p>
+    </section>
+    <section className='items'>
+      {data.skills.map((e, index) => {
+        return (
+          <ItemSkills
+            key={index}
+            name={e.name}
+            image={e.image}
+          />
+        );
+      })}
+    </section>
+    <section className='title'>
+      <p className='title-skills'>COMPETENCIAS TÉCNICAS</p>
+    </section>
+  </section>
 );
 
 export default Skills;
