38c0b4a33f70015725b0992659f1b9beceb794e6
DianaMS <dmartinezs@uni.pe>
2020-08-24 19:37:19 -0500

    creación de data para la app

---
+++ src/data/data.js
@@ -0,0 +1,136 @@
+export default {
+  'projects': [{
+    'gif': 'src/assets/static/cardValidation.gif',
+    'title': 'CARD VALIDATION',
+    'description': 'Aquí va la descripción',
+    'buttonleft': 'README',
+    'buttonright': 'DESPLIEGUE',
+  },
+  {
+    'gif': 'src/assets/static/cipher.gif',
+    'title': 'CIPHER',
+    'description': 'Aquí va la descripción',
+    'buttonleft': 'README',
+    'buttonright': 'DESPLIEGUE',
+  },
+  {
+    'gif': 'src/assets/static/pokemonGo.gif',
+    'title': 'POKEMON GO',
+    'description': 'Aquí va la descripción',
+    'buttonleft': 'README',
+    'buttonright': 'DESPLIEGUE',
+  },
+  {
+    'gif': 'src/assets/static/mdLinks.gif',
+    'title': 'MD-LINKS',
+    'description': 'Aquí va la descripción',
+    'buttonleft': 'README',
+    'buttonright': 'DESPLIEGUE',
+  },
+  {
+    'gif': 'src/assets/static/redSocial.gif',
+    'title': 'RED SOCIAL',
+    'description': 'Aquí va la descripción',
+    'buttonleft': 'README',
+    'buttonright': 'DESPLIEGUE',
+  },
+  {
+    'gif': 'src/assets/static/burgerApi.gif',
+    'title': 'BURGER QUEEN API',
+    'description': 'Aquí va la descripción',
+    'buttonleft': 'README',
+    'buttonright': 'DESPLIEGUE',
+  }],
+  'skills': [{
+    'name': 'Figma',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101815-7fc8bc00-e62d-11ea-9582-a318200c272d.png',
+  },
+  {
+    'name': 'Canva',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101826-85260680-e62d-11ea-8b68-59a70e18a44c.png',
+  },
+  {
+    'name': 'Adobe Photoshop',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101834-8a835100-e62d-11ea-9642-ee646b8b88b7.png',
+  },
+  {
+    'name': 'Adobe Illustrator',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101843-8d7e4180-e62d-11ea-964a-157fd2fa67f8.png',
+  },
+  {
+    'name': 'Trello',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101847-90793200-e62d-11ea-866b-c359b7d0086b.png',
+  },
+  {
+    'name': 'JavaScript',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101862-98d16d00-e62d-11ea-8401-7bee0e4148f8.png',
+  },
+  {
+    'name': 'HTML5',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101868-9b33c700-e62d-11ea-9083-d45d26bc41cb.png',
+  },
+  {
+    'name': 'CSS3',
+    'image': 'https://user-images.githubusercontent.com/60928490/91101878-9d962100-e62d-11ea-9237-dd655209d089.png',
+  },
+  {
+    'name': 'TypeScript',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102255-6ecc7a80-e62e-11ea-8564-6de69450a6fb.png',
+  },
+  {
+    'name': 'NodeJS',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102261-71c76b00-e62e-11ea-851b-416ec82ec1b8.png',
+  },
+  {
+    'name': 'Express',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102309-86a3fe80-e62e-11ea-9979-76d09b2cec8c.png',
+  },
+  {
+    'name': 'Sass',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102314-8a378580-e62e-11ea-85fd-d178fffe8e06.png',
+  },
+  {
+    'name': 'React',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102321-8d327600-e62e-11ea-8a20-9305cd0c7116.png',
+  },
+  {
+    'name': 'Angular',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102331-915e9380-e62e-11ea-9821-b749bd9dcb27.png',
+  },
+  {
+    'name': 'Firebase',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102342-97ed0b00-e62e-11ea-8ba3-39beb768b28b.png',
+  },
+  {
+    'name': 'MongoDB',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102347-9d4a5580-e62e-11ea-938c-abb5ffcf18b6.png',
+  },
+  {
+    'name': 'AWS',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102356-a0dddc80-e62e-11ea-98e1-53dbfb39e136.png',
+  },
+  {
+    'name': 'Docker',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102365-a5a29080-e62e-11ea-8e34-d91f7b7cc17b.png',
+  },
+  {
+    'name': 'Webpack',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102375-a9ceae00-e62e-11ea-8985-136d868b7467.png',
+  },
+  {
+    'name': 'ESLINT',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102383-ac310800-e62e-11ea-9fb3-2727326e355a.png',
+  },
+  {
+    'name': 'Git',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102392-b0f5bc00-e62e-11ea-9676-f990132f138e.png',
+  },
+  {
+    'name': 'GitHub',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102398-b3581600-e62e-11ea-9cb2-e08ca858e4f1.png',
+  },
+  {
+    'name': 'Postman',
+    'image': 'https://user-images.githubusercontent.com/60928490/91102401-b521d980-e62e-11ea-8839-61041920bff1.png',
+  }],
+};
